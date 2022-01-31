960b86be71df0fc3603684ba45eb712d96e0e521
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-01-31 23:25:19 +0000

    Feat: Adds theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
 import "../styles/globals.css";
 
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider, createTheme, Theme } from "@nextui-org/react";
+
+const myTheme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: "$green200",
+      primary: "#4ADE7B",
+      primaryDark: "$green600",
+
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+      link: "#5E1DAD",
+
+      // you can also create your own color
+      myColor: "#ff4ecd",
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {},
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={myTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
