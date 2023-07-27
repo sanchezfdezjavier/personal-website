47df30236b965d5141db02853d83a8f163fe5f9c
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-07-27 13:16:23 +0200

    Improves mobile view and minor enhacemennts

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Avatar() {
   return (
-    <div className="relative mb-12 mt-[6rem] block h-[14rem] w-[14rem] items-center justify-center lg:mt-[12rem] lg:h-[17rem] lg:w-[17rem]">
+    <div className="relative mb-12 mt-[4rem] block h-[14rem] w-[14rem] items-center justify-center lg:mt-[12rem] lg:h-[17rem] lg:w-[17rem]">
       <Image
         src={avatarSrc}
         className="rounded-[3rem]"
--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import { footerText } from "../config";
 
 export default function Footer() {
   return (
-    <div className="mt-[8rem] flex w-screen flex-col items-center bg-gray-100 py-[2rem] text-slate-400">
+    <div className="mt-[8rem] flex w-screen flex-col items-center bg-gradient-to-t from-slate-200 py-[2rem] text-slate-400">
       <div className="flex w-11/12 flex-row items-center justify-center">
         <span className="mr-1 inline-block">Built using</span>
         <span className="m-1 inline-block">
