286a71a8a6d2d74131e504e0dfaba7d3bcd54cd2
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-11-25 01:19:05 +0100

    [Feat]: Adds posthog

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -9,7 +9,7 @@ export default function CallToAction() {
         href={externalLinks.stackai}
         target="_blank"
         rel="noreferrer"
-        className="flex w-full rounded-xl bg-slate-900 px-[5rem] py-[1.5rem] text-xl font-medium text-white shadow-lg shadow-slate-800/50 transition-all hover:-translate-y-1 hover:bg-slate-800 md:w-fit"
+        className="flex w-full rounded-xl bg-black px-[5rem] py-[1.5rem] text-xl font-light text-white shadow shadow-slate-800/50 transition-all hover:bg-gray-900 md:w-fit"
       >
         <span>{mainButtonText}</span>
       </Link>
@@ -17,7 +17,7 @@ export default function CallToAction() {
         href={externalLinks.blog}
         target="_blank"
         rel="noreferrer"
-        className="flex w-full items-center rounded-xl px-[5rem] py-[1.5rem] text-xl font-medium text-gray-900 transition-all hover:underline md:w-fit"
+        className="flex w-full items-center rounded-xl px-[5rem] py-[1.5rem] text-xl text-black transition-all hover:underline md:w-fit"
       >
         {secondaryButtonText}
         <HiOutlineExternalLink className="ml-2 inline-block stroke-2" />
--- components/SectionTitle.js
+++ components/SectionTitle.js
@@ -1,6 +1,6 @@
 export default function SectionTitle(props) {
   return (
-    <h2 className="mb-8 mt-10 text-5xl font-bold tracking-tight text-slate-900">
+    <h2 className="mb-8 mt-10 text-5xl font-bold tracking-tight text-black">
       {props.children}
     </h2>
   );
